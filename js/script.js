ce988cba2a818da350b7ab38573b7f808e5d9ef5
Florian <florian@alonso.im>
2019-08-07 17:28:45 +0200

    Brouillon de la base.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let gameDisplay = {
+    canvas : document.querySelector("#game-display")
+}
+
+
+
+display.width = 500;
+display.height = 350;
+
+
+class GameDisplay {
+    constructor(canvas) {
+        this.canvas = document.querySelector("#game-display");
+    }
+}
